3cd276b33ee17396229a5f62a773c8df70839c54
Matthew Harding <matthewpharding@gmail.com>
2023-11-22 18:03:56 +0000

    Completed Topic 6 Functions.

--- The Swift Cheat Sheet.playground/Pages/6 Functions.xcplaygroundpage/Contents.swift
+++ The Swift Cheat Sheet.playground/Pages/6 Functions.xcplaygroundpage/Contents.swift
@@ -539,14 +539,15 @@ print("zero!")
 //:
 //: ## Nested Functions
 //:
-//: All of the functions you have encountered so far in this chapter have been examples of global functions, which are defined at a global scope. You can also define functions inside the bodies of other functions, known as nested functions.
-//:
-//: Nested functions are hidden from the outside world by default, but can still be called and used by their enclosing function. An enclosing function can also return one of its nested functions to allow the nested function to be used in another scope.
-//:
-//: You can rewrite the chooseStepFunction(backward:) example above to use and return nested functions:
+//: Functions can contain nested functions:
 func chooseStepFunction2(backward: Bool) -> (Int) -> Int {
-    func stepForward(input: Int) -> Int { return input + 1 }
-    func stepBackward(input: Int) -> Int { return input - 1 }
+    func stepForward(input: Int) -> Int { // nested func 1
+        return input + 1
+    }
+    func stepBackward(input: Int) -> Int { // nested func 2
+        return input - 1
+        
+    }
     return backward ? stepBackward : stepForward
 }
 currentValue = -4
