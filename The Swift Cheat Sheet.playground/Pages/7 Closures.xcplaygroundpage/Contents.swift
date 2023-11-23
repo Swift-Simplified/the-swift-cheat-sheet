b42d7bfbae0354327bd0e3efbab272911742db6c
Matthew Harding <matthewpharding@gmail.com>
2023-11-23 17:57:06 +0000

    Completed the middle lessons of topic 7.
    Topic 7 is closures.

--- The Swift Cheat Sheet.playground/Pages/7 Closures.xcplaygroundpage/Contents.swift
+++ The Swift Cheat Sheet.playground/Pages/7 Closures.xcplaygroundpage/Contents.swift
@@ -251,15 +251,19 @@ loadPicture(from: someServer) { picture in  // trailing closure 1
 //:
 //: ## Capturing Values
 //:
-//: A closure can capture constants and variables from the surrounding context in which it’s defined. The closure can then refer to and modify the values of those constants and variables from within its body, even if the original scope that defined the constants and variables no longer exists.
+//: A closure can capture constants and variables from the surrounding context in which it’s defined. 
 //:
-//: In Swift, the simplest form of a closure that can capture values is a nested function, written within the body of another function. A nested function can capture any of its outer function’s arguments and can also capture any constants and variables defined within the outer function.
+//: The closure can then refer to and modify the values of those constants and variables, even if the original scope that defined the constants and variables no longer exists.
 //:
-//: Here’s an example of a function called makeIncrementer, which contains a nested function called incrementer. The nested incrementer() function captures two values, runningTotal and amount, from its surrounding context. After capturing these values, incrementer is returned by makeIncrementer as a closure that increments runningTotal by amount each time it’s called.
+//: -------------------
+//:
+//: The simplest form of a closure that can capture values is a nested function.
+//:
+//: Here’s an example of a nested function, which captures two values, runningTotal and amount:
 func makeIncrementer(forIncrement amount: Int) -> () -> Int {
     var runningTotal = 0
     func incrementer() -> Int {
-        runningTotal += amount
+        runningTotal += amount // both are captured here
         return runningTotal
     }
     return incrementer
@@ -268,84 +272,70 @@ func makeIncrementer(forIncrement amount: Int) -> () -> Int {
 //:
 //: -------------------
 //:
-//: The return type of makeIncrementer is () -> Int. This means that it returns a function, rather than a simple value. The function it returns has no parameters, and returns an Int value each time it’s called. To learn how functions can return other functions, see Function Types as Return Types.
-//:
-//: The makeIncrementer(forIncrement:) function defines an integer variable called runningTotal, to store the current running total of the incrementer that will be returned. This variable is initialized with a value of 0.
-//:
-//: The makeIncrementer(forIncrement:) function has a single Int parameter with an argument label of forIncrement, and a parameter name of amount. The argument value passed to this parameter specifies how much runningTotal should be incremented by each time the returned incrementer function is called. The makeIncrementer function defines a nested function called incrementer, which performs the actual incrementing. This function simply adds amount to runningTotal, and returns the result.
-//:
-//: When considered in isolation, the nested incrementer() function might seem unusual:
-// ⛔️ Compilation error: uncomment to view
-//func incrementer() -> Int {
-//    runningTotal += amount
-//    return runningTotal
-//}
+//: The return type of makeIncrementer is () -> Int. This means that it returns a function.
+let anIncrementFunction = makeIncrementer(forIncrement: 1)
 // << 🔵 Run Point
 //:
 //: -------------------
 //:
-//: The incrementer() function doesn’t have any parameters, and yet it refers to runningTotal and amount from within its function body. It does this by capturing a reference to runningTotal and amount from the surrounding function and using them within its own function body. Capturing by reference ensures that runningTotal and amount don’t disappear when the call to makeIncrementer ends, and also ensures that runningTotal is available the next time the incrementer function is called.
-//:
-//: * callout(Note):
-//:     → As an optimization, Swift may instead capture and store a copy of a value if that value isn’t mutated by a closure, and if the value isn’t mutated after the closure is created.
-//:
-//:     Swift also handles all memory management involved in disposing of variables when they’re no longer needed.
-//:
-//: Here’s an example of makeIncrementer in action:
-let incrementByTen = makeIncrementer(forIncrement: 10)
+//: The function returns an Int value each time it’s called:
+let value1 = anIncrementFunction() // returns 1
+let value2 = anIncrementFunction() // returns 2
+let value3 = anIncrementFunction() // returns 3
 // << 🔵 Run Point
 //:
 //: -------------------
 //:
-//: This example sets a constant called incrementByTen to refer to an incrementer function that adds 10 to its runningTotal variable each time it’s called. Calling the function multiple times shows this behavior in action:
-incrementByTen()
-// returns a value of 10
-incrementByTen()
-// returns a value of 20
-incrementByTen()
-// returns a value of 30
+//: The runningTotal variable was captured by the nested function.
+//:
+//: An area of memory for this value will exist until all references are disconnected.
+//:
+//: -------------------
+//:
+//: This variable starts at 0 and increments with each function call:
+//:
+let incrementByTen = makeIncrementer(forIncrement: 10) // increment by 10
+
+var num = incrementByTen()  // returns a value of 10
+num = incrementByTen()      // returns a value of 20
+num = incrementByTen()      // returns a value of 30
+print(num)
 // << 🔵 Run Point
 //:
 //: -------------------
 //:
-//: If you create a second incrementer, it will have its own stored reference to a new, separate runningTotal variable:
+//: When you create a second incrementer, it will create a new, separate runningTotal variable:
 let incrementBySeven = makeIncrementer(forIncrement: 7)
-incrementBySeven()
-// returns a value of 7
+num = incrementBySeven() // returns a value of 7
+num = incrementBySeven() // returns a value of 14
 // << 🔵 Run Point
 //:
 //: -------------------
 //:
-//: Calling the original incrementer (incrementByTen) again continues to increment its own runningTotal variable, and doesn’t affect the variable captured by incrementBySeven:
-incrementByTen()
-// returns a value of 40
+//: Each function increments its own runningTotal variable and no others:
+num = incrementByTen() // returns a value of 40
+num = incrementByTen() // returns a value of 50
+
+num = incrementBySeven() // returns a value of 21
+num = incrementBySeven() // returns a value of 28
 // << 🔵 Run Point
-//:
-//: -------------------
-//:
-//: * callout(Note):
-//:     → If you assign a closure to a property of a class instance, and the closure captures that instance by referring to the instance or its members, you will create a strong reference cycle between the closure and the instance. Swift uses capture lists to break these strong reference cycles. For more information, see Strong Reference Cycles for Closures.
 //:
 //: -------------------
 //:
 //: ## Closures Are Reference Types
 //:
-//: In the example above, incrementBySeven and incrementByTen are constants, but the closures these constants refer to are still able to increment the runningTotal variables that they have captured. This is because functions and closures are reference types.
+//: Functions and closures are reference types.
 //:
-//: Whenever you assign a function or a closure to a constant or a variable, you are actually setting that constant or variable to be a reference to the function or closure. In the example above, it’s the choice of closure that incrementByTen refers to that’s constant, and not the contents of the closure itself.
-//:
-//: This also means that if you assign a closure to two different constants or variables, both of those constants or variables refer to the same closure.
-let alsoIncrementByTen = incrementByTen
-alsoIncrementByTen()
-// returns a value of 50
+//: All references point to the same closure:
+let incrementByFive = makeIncrementer(forIncrement: 5) // create one incrementer
+let sameClosure = incrementByFive
+let alsoSameClosure = sameClosure
 
-incrementByTen()
-// returns a value of 60
+incrementByFive()   // 5
+sameClosure()       // 10
+alsoSameClosure()   // 15
+// all variables point to the same closure!
 // << 🔵 Run Point
-//:
-//: -------------------
-//:
-//: The example above shows that calling alsoIncrementByTen is the same as calling incrementByTen. Because both of them refer to the same closure, they both increment and return the same running total.
 //:
 //: -------------------
 //:
