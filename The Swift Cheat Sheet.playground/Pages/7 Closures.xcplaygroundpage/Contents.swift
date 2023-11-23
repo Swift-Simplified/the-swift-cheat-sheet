64628c1810f63cf8ed6deba4adc6710fd2598011
Matthew Harding <matthewpharding@gmail.com>
2023-11-23 16:27:33 +0000

    Converted some more lessons from Topic 7.
    Topic 7 is closures.

--- The Swift Cheat Sheet.playground/Pages/7 Closures.xcplaygroundpage/Contents.swift
+++ The Swift Cheat Sheet.playground/Pages/7 Closures.xcplaygroundpage/Contents.swift
@@ -135,8 +135,11 @@ reversedNames = names.sorted(by: { $0 > $1 } )
 //:
 //: ### Operator Methods
 //:
-//: There’s actually an even shorter way to write the closure expression above. Swift’s String type defines its string-specific implementation of the greater-than operator (>) as a method that has two parameters of type String, and returns a value of type Bool. This exactly matches the method type needed by the sorted(by:) method. Therefore, you can simply pass in the greater-than operator, and Swift will infer that you want to use its string-specific implementation:
-reversedNames = names.sorted(by: >)
+//: Operators are actually functions.
+//:
+//: Use operators as parameter values to refer to their functions:
+let alphabeticalOrder = names.sorted(by: <)
+let reverseAlphabeticalOrder = names.sorted(by: >)
 // << 🔵 Run Point
 //:
 //: -------------------
@@ -147,55 +150,54 @@ reversedNames = names.sorted(by: >)
 //:
 //: ## Trailing Closures
 //:
-//: If you need to pass a closure expression to a function as the function’s final argument and the closure expression is long, it can be useful to write it as a trailing closure instead. You write a trailing closure after the function call’s parentheses, even though the trailing closure is still an argument to the function. When you use the trailing closure syntax, you don’t write the argument label for the first closure as part of the function call. A function call can include multiple trailing closures; however, the first few examples below use a single trailing closure.
+//: When a function’s final argument accepts a function it can be called with a trailing closure.
+//:
+//: You omit the parameter label and append a closure to the call:
 func someFunctionThatTakesAClosure(closure: () -> Void) {
-    // function body goes here
+    // do something
 }
 
-// Here's how you call this function without using a trailing closure:
-
+// without using a trailing closure
 someFunctionThatTakesAClosure(closure: {
-    // closure's body goes here
+    // closure code
 })
 
-// Here's how you call this function with a trailing closure instead:
-
+// with a trailing closure instead
 someFunctionThatTakesAClosure() {
-    // trailing closure's body goes here
+    // closure code
+}
+
+// trailing closure - no parenthesis
+someFunctionThatTakesAClosure {
+    // closure code
 }
 // << 🔵 Run Point
 //:
 //: -------------------
 //:
-//: The string-sorting closure from the Closure Expression Syntax section above can be written outside of the sorted(by:) method’s parentheses as a trailing closure:
+//: The sorted(by:) method can use a trailing closure:
 reversedNames = names.sorted() { $0 > $1 }
 // << 🔵 Run Point
 //:
 //: -------------------
 //:
-//: If a closure expression is provided as the function’s or method’s only argument and you provide that expression as a trailing closure, you don’t need to write a pair of parentheses () after the function or method’s name when you call the function:
+//: When a function has one argument the parentheses () are optional:
 reversedNames = names.sorted { $0 > $1 } // this is the preferred Swift style
+reversedNames = names.sorted() { $0 > $1 }
 // << 🔵 Run Point
 //:
 //: -------------------
 //:
-//: Trailing closures are most useful when the closure is sufficiently long that it isn’t possible to write it inline on a single line. As an example, Swift’s Array type has a map(_:) method, which takes a closure expression as its single argument. The closure is called once for each item in the array, and returns an alternative mapped value (possibly of some other type) for that item. You specify the nature of the mapping and the type of the returned value by writing code in the closure that you pass to map(_:).
+//: The map(_:) method is used to convert an array of values to a different type.
 //:
-//: After applying the provided closure to each array element, the map(_:) method returns a new array containing all of the new mapped values, in the same order as their corresponding values in the original array.
+//: This method is often used with a trailing closure:
 //:
-//: Here’s how you can use the map(_:) method with a trailing closure to convert an array of Int values into an array of String values. The array [16, 58, 510] is used to create the new array ["OneSix", "FiveEight", "FiveOneZero"]:
 let digitNames = [
     0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
     5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
 ]
 let numbers = [16, 58, 510]
-// << 🔵 Run Point
-//:
-//: -------------------
-//:
-//: The code above creates a dictionary of mappings between the integer digits and English-language versions of their names. It also defines an array of integers, ready to be converted into strings.
-//:
-//: You can now use the numbers array to create an array of String values, by passing a closure expression to the array’s map(_:) method as a trailing closure:
+
 let strings = numbers.map { (number) -> String in
     var number = number
     var output = ""
@@ -207,28 +209,14 @@ let strings = numbers.map { (number) -> String in
 }
 // strings is inferred to be of type [String]
 // its value is ["OneSix", "FiveEight", "FiveOneZero"]
+print(strings)
 // << 🔵 Run Point
 //:
 //: -------------------
 //:
-//: The map(_:) method calls the closure expression once for each item in the array. You don’t need to specify the type of the closure’s input parameter, number, because the type can be inferred from the values in the array to be mapped.
+//: Multiple trailing closures are supported.
 //:
-//: In this example, the variable number is initialized with the value of the closure’s number parameter, so that the value can be modified within the closure body. (The parameters to functions and closures are always constants.) The closure expression also specifies a return type of String, to indicate the type that will be stored in the mapped output array.
-//:
-//: The closure expression builds a string called output each time it’s called. It calculates the last digit of number by using the remainder operator (number % 10), and uses this digit to look up an appropriate string in the digitNames dictionary. The closure can be used to create a string representation of any integer greater than zero.
-//:
-//: * callout(Note):
-//:     → The call to the digitNames dictionary’s subscript is followed by an exclamation point (!), because dictionary subscripts return an optional value to indicate that the dictionary lookup can fail if the key doesn’t exist. In the example above, it’s guaranteed that number % 10 will always be a valid subscript key for the digitNames dictionary, and so an exclamation point is used to force-unwrap the String value stored in the subscript’s optional return value.
-//:
-//: The string retrieved from the digitNames dictionary is added to the front of output, effectively building a string version of the number in reverse. (The expression number % 10 gives a value of 6 for 16, 8 for 58, and 0 for 510.)
-//:
-//: The number variable is then divided by 10. Because it’s an integer, it’s rounded down during the division, so 16 becomes 1, 58 becomes 5, and 510 becomes 51.
-//:
-//: The process is repeated until number is equal to 0, at which point the output string is returned by the closure, and is added to the output array by the map(_:) method.
-//:
-//: The use of trailing closure syntax in the example above neatly encapsulates the closure’s functionality immediately after the function that closure supports, without needing to wrap the entire closure within the map(_:) method’s outer parentheses.
-//:
-//: If a function takes multiple closures, you omit the argument label for the first trailing closure and you label the remaining trailing closures. For example, the function below loads a picture for a photo gallery:
+//: You omit the first argument label and you label the remaining trailing closures:
 class Server {}
 class Picture {}
 
@@ -237,6 +225,7 @@ func download(_ imageName: String, from: Server) -> Picture? {
     return Picture()
 }
 
+// accept two functions as parameters
 func loadPicture(from server: Server, completion: (Picture) -> Void, onFailure: () -> Void) {
     if let picture = download("photo.jpg", from: server) {
         completion(picture)
@@ -244,30 +233,19 @@ func loadPicture(from server: Server, completion: (Picture) -> Void, onFailure: 
         onFailure()
     }
 }
-// << 🔵 Run Point
-//:
-//: -------------------
-//:
-//: When you call this function to load a picture, you provide two closures. The first closure is a completion handler that displays a picture after a successful download. The second closure is an error handler that displays an error to the user.
+
 class SomeView {
     var currentPicture: Picture?
 }
 
 let someView = SomeView()
 let someServer = Server()
-loadPicture(from: someServer) { picture in
+loadPicture(from: someServer) { picture in  // trailing closure 1
     someView.currentPicture = picture
-} onFailure: {
+} onFailure: {                              // trailing closure 2
     print("Couldn't download the next picture.")
 }
 // << 🔵 Run Point
-//:
-//: -------------------
-//:
-//: In this example, the loadPicture(from:completion:onFailure:) function dispatches its network task into the background, and calls one of the two completion handlers when the network task finishes. Writing the function this way lets you cleanly separate the code that’s responsible for handling a network failure from the code that updates the user interface after a successful download, instead of using just one closure that handles both circumstances.
-//:
-//: * callout(Note):
-//:     → Completion handlers can become hard to read, especially when you have to nest multiple handlers. An alternate approach is to use asynchronous code, as described in Concurrency.
 //:
 //: -------------------
 //:
